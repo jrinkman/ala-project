5c0fd37e4a23089225fc1520b14502f3298c2f7a
Jack Brinkman <jack.ber.private@gmail.com>
2022-03-05 00:02:23 +1000

    Added searching flag, fixed CSV button styling on mobile

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import SpeciesCard from './components/SpeciesCard';
 
 function Main() {
   const [speciesData, setSpeciesData] = useState<SearchResults | null>(null);
+  const [isSearching, setIsSearching] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
 
   // Listen for search changes <FIX>
@@ -17,17 +18,18 @@ function Main() {
       try {
         // Reset the error state (in case we're making a new request after an error)
         setError(null);
+        setIsSearching(true);
 
         // Make a request to the ALA API
         const { searchResults } = (
           await axios.get<SearchResponse>(`search.json`)
         ).data;
 
-        console.log(searchResults);
-
+        setIsSearching(false);
         setSpeciesData(searchResults);
       } catch (error) {
         setError(error as Error);
+        setIsSearching(false);
       }
     }
 
@@ -36,25 +38,30 @@ function Main() {
 
   return (
     <Container size="xl">
-      <Card shadow="lg" radius="lg">
-        <Group spacing="sm">
-          <TextInput
-            sx={{ input: { fontFamily: 'Inter' }, flexGrow: 1 }}
-            placeholder="Species name, i.e. 'Fish'"
-            onChange={(ev) => console.log(ev)}
-            radius="md"
-          />
-          <Button
-            disabled={!speciesData || speciesData.results.length === 0}
-            radius="md"
-            variant="light"
-            leftIcon={<DownloadIcon />}
-          >
-            CSV
-          </Button>
-        </Group>
+      <Card shadow="md" radius="lg" style={{ marginTop: 15, marginBottom: 35 }}>
+        <Grid>
+          <Grid.Col xs={12} sm={10} md={10} lg={10} xl={11}>
+            <TextInput
+              sx={{ input: { fontFamily: 'Inter' } }}
+              placeholder="Species name, i.e. 'Fish'"
+              onChange={(ev) => console.log(ev)}
+              radius="md"
+            />
+          </Grid.Col>
+          <Grid.Col xs={12} sm={2} md={2} lg={2} xl={1}>
+            <Button
+              loading={!speciesData || speciesData.results.length === 0}
+              radius="md"
+              variant="light"
+              leftIcon={<DownloadIcon />}
+              fullWidth
+            >
+              CSV
+            </Button>
+          </Grid.Col>
+        </Grid>
       </Card>
-      {speciesData ? (
+      {speciesData && !isSearching ? (
         <Grid>
           {speciesData.results.map((species: Species) => (
             <Grid.Col key={species.id} xs={12} sm={6} md={4} lg={3}>
