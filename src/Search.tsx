7113b0816770c9cca40278bf508e4f5b7077b457
Jack Brinkman <jack.ber.private@gmail.com>
2022-03-06 15:01:35 +1000

    First working search pass, added pagination

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,8 +1,9 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import {
   TextInput,
   ScrollArea,
   Grid,
+  Group,
   Text,
   Button,
   Card,
@@ -10,6 +11,7 @@ import {
   Container,
   Pagination,
   Title,
+  ActionIcon,
 } from '@mantine/core';
 import { DownloadIcon, MagnifyingGlassIcon } from '@radix-ui/react-icons';
 
@@ -21,37 +23,45 @@ import Message from './components/Message';
 
 function Search() {
   const [speciesData, setSpeciesData] = useState<SearchResults | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [isSearching, setIsSearching] = useState<boolean>(false);
   const [isExporting, setIsExporting] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  // Listen for search changes <FIX>
-  useEffect(() => {
-    async function getSearchResults() {
-      try {
-        // Reset the error state (in case we're making a new request after an error)
-        setError(null);
-        setIsSearching(true);
+  // Store a reference to the search input box
+  const searchInputRef = useRef<HTMLInputElement>(null);
 
-        // Make a request to the ALA API
-        const { searchResults } = (
-          await axios.get<SearchResponse>(`search.json`)
-        ).data;
+  // Callback funciton for searching the API
+  const getSearchResults = useCallback(async () => {
+    // Ensure the search box has an input value
+    if (!searchInputRef.current || searchInputRef.current.value.length < 1)
+      return;
 
-        setIsSearching(false);
-        setSpeciesData(searchResults);
-      } catch (error) {
-        if (axios.isAxiosError(error)) {
-          setError(`Network Error: ${(error as AxiosError).message}`);
-        } else setError((error as Error).message);
+    try {
+      // Reset the error state (in case we're making a new request after an error)
+      setError(null);
+      setIsSearching(true);
 
-        // Disable the searching flag
-        setIsSearching(false);
-      }
+      // Make a request to the ALA API
+      const { searchResults } = (
+        await axios.get<SearchResponse>(
+          `search.json?q=${
+            searchInputRef.current.value
+          }&pageSize=100&recordOffset=${currentPage - 1}`
+        )
+      ).data;
+
+      setIsSearching(false);
+      setSpeciesData(searchResults);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setError(`Network Error: ${(error as AxiosError).message}`);
+      } else setError((error as Error).message);
+
+      // Disable the searching flag
+      setIsSearching(false);
     }
-
-    getSearchResults();
-  }, []);
+  }, [searchInputRef, isSearching]);
 
   // Callback function for downloading the results as a CSV
   const exportResultsCSV = useCallback(() => {
@@ -72,17 +82,31 @@ function Search() {
         paddingTop: 15,
       }}
     >
-      <Card shadow="md" radius="lg" style={{ marginBottom: 35 }}>
+      <Card shadow="md" radius="lg" style={{ overflow: 'visible' }}>
         <Grid>
           <Grid.Col xs={12} sm={10} md={10} lg={10} xl={11}>
-            <TextInput
-              icon={<MagnifyingGlassIcon />}
-              disabled={isSearching}
-              sx={{ input: { fontFamily: 'Inter' } }}
-              placeholder="Species name, i.e. 'Fish'"
-              onChange={(ev) => console.log(ev)}
-              radius="md"
-            />
+            <Group>
+              <TextInput
+                ref={searchInputRef}
+                icon={<MagnifyingGlassIcon />}
+                disabled={isSearching}
+                sx={{ input: { fontFamily: 'Inter' }, flexGrow: 1 }}
+                placeholder="Species name, i.e. 'Fish'"
+                onKeyPress={(ev) => {
+                  if (ev.key === 'Enter') getSearchResults();
+                }}
+                radius="md"
+              />
+              <ActionIcon
+                variant="light"
+                color="blue"
+                size="lg"
+                radius="md"
+                onClick={getSearchResults}
+              >
+                <MagnifyingGlassIcon />
+              </ActionIcon>
+            </Group>
           </Grid.Col>
           <Grid.Col xs={12} sm={2} md={2} lg={2} xl={1}>
             <Button
@@ -109,15 +133,21 @@ function Search() {
           );
         } else if (isSearching) {
           // If an API request is in progress
-          return Array.from(Array(15).keys()).map((key) => (
-            <Grid.Col key={key} xs={12} sm={6} md={4} lg={3}>
-              <SpeciesCard species={null} />
-            </Grid.Col>
-          ));
+          return (
+            <ScrollArea offsetScrollbars style={{ flexGrow: 1, marginTop: 35 }}>
+              <Grid>
+                {Array.from(Array(15).keys()).map((key) => (
+                  <Grid.Col key={key} xs={12} sm={6} md={4} lg={3}>
+                    <SpeciesCard species={null} />
+                  </Grid.Col>
+                ))}
+              </Grid>
+            </ScrollArea>
+          );
         } else if (speciesData) {
           // If we have data from a successful API request
           return speciesData.results.length > 0 ? (
-            <ScrollArea offsetScrollbars style={{ flexGrow: 1 }}>
+            <ScrollArea offsetScrollbars style={{ flexGrow: 1, marginTop: 35 }}>
               <Grid>
                 {speciesData.results.map((species: Species) => (
                   <Grid.Col key={species.id} xs={12} sm={6} md={4} lg={3}>
@@ -144,6 +174,11 @@ function Search() {
       {speciesData && speciesData.results.length > 0 && (
         <Center style={{ paddingTop: 30, paddingBottom: 30 }}>
           <Pagination
+            page={currentPage}
+            onChange={(newPage) => {
+              setCurrentPage(newPage);
+              getSearchResults();
+            }}
             radius="md"
             total={Math.floor(speciesData.totalRecords / 100) + 1}
             styles={{ item: { fontFamily: 'Inter' } }}
